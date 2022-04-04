df0505d0d398d2ccce5a0c31989b5fd93eb21959
matheuscms95 <matheuscms95@gmail.com>
2022-04-04 14:39:35 -0300

    14:37

---
+++ IMC - VS/IMC - VS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IMC___VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura, peso, imc;
+            
+            Console.WriteLine("Olá digite aqui o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Entre com sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+            Console.WriteLine("Temos o resultado!" + "\n" + nome + "." + " O seu IMC atual é: " + imc);
+        }
+    }
+}
