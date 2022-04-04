66d715518da57654087c60e3eb9c12acbd19d3ff
matheuscms95 <matheuscms95@gmail.com>
2022-04-04 11:52:52 -0300

    @matheuscms95 aula 3 dia 04042022

---
+++ Alua3VSC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Alua3VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float peso;
+            float imc;
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+            
+
+        }
+    }
+}
