ce17a6c2057a4cf04328ffa5adab725dab052c2c
matheuscms95 <matheuscms95@gmail.com>
2022-04-05 12:51:06 -0300

    @matheuscms95 [Aula: 5]

---
+++ Aula5VSC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula5VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com valor vendido mensal: ");
+            double valorVendido = double.Parse(Console.ReadLine());
+
+            if(valorVendido <= 10000)
+            {
+                Console.WriteLine("O valor a receber: R$" + (valorVendido * 0.01));
+            } 
+            else if (valorVendido <= 20000)
+            {
+                Console.WriteLine("O valor a receber: R$" + (valorVendido * 0.02));
+            }
+            else if (valorVendido <= 30000)
+            {
+                Console.WriteLine("O valor a receber: R$" + (valorVendido * 0.03));
+            }  
+            else
+            {
+                Console.WriteLine("O valor a receber: R$" + (valorVendido * 0.1));
+            }         
+        }
+
+    }
+}
