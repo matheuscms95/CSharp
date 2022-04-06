1fcf16061f8cb0b8134a961b09c2fba084aaead4
matheuscms95 <matheuscms95@gmail.com>
2022-04-06 12:53:09 -0300

    Aula 06/04/2022

---
+++ Aula6VS/Aula6VS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula6VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            for(int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"Posição {i}.\nEntre com o valor que deseja somar a {valor}: ");
+                valor += int.Parse(Console.ReadLine());
+                // += == Valor = valor + int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Somatoria: {valor}");
+        }
+    }
+}
---
+++ Aula6VSC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula6VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+
+            for(int x = 1; x <= 10; x++)
+            {
+                for(int y = 1; y <= 10; y++)
+                {
+                    //Console.WriteLine(x + " x " + y + " = " + x * y);
+                    Console.WriteLine($"{x} x {y} = {x * y}");
+                    
+                    if(y == 5)
+                    {
+                        break;
+                    }             
+                }
+            }   
+        }
+    }
+}
