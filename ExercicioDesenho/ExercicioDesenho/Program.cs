9046f741d00d12caa71afba3a3e49372139b4f88
matheuscms95 <matheuscms95@gmail.com>
2022-04-08 22:22:27 -0300

    Atividade 08/04/2022

---
+++ ExercicioDesenho/ExercicioDesenho/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExercicioDesenho.src;
+using System;
+
+namespace ExercicioDesenho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atividade 08/04/2022");
+            Console.WriteLine("Exercício de Desenho");
+            
+            
+            Cachorro floquinho = new Cachorro("Floquinho", 5, "azul", "Pequeno");
+            Console.WriteLine($"O nome do cachorro é {floquinho.Nome}, O peso é {floquinho.Peso}kg, A cor é {floquinho.Cor}, O tamanho é {floquinho.Tamanho}.");
+
+            floquinho.Comer();
+
+            Cachorro beth = new Cachorro("Beth", 9, "Preta", "Pequena");
+            Console.WriteLine($"O nome do cachorro é {beth.Nome}, O peso é {beth.Peso}kg, A cor é {beth.Cor}, O tamanho é {beth.Tamanho}.");
+            
+            beth.Dormir();
+           
+
+        }
+    }
+}
---
+++ ExercicioDesenho/ExercicioDesenho/src/Cachorro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDesenho.src
+{
+    public class Cachorro 
+    {
+        public string Nome { get; set; }
+        public int Peso { get; set; }
+        public string Cor { get; set; }
+        public string Tamanho { get; set; }
+        
+        public Cachorro (string _nome, int _peso, string _cor, string _tamanho) 
+        {
+            Nome = _nome;
+            Peso = _peso;
+            Cor = _cor;
+            Tamanho = _tamanho;
+        }
+        
+        public void Comer() 
+        {
+            Console.WriteLine("Estou comendo, ai que delícia.");
+        }
+
+        public void Dormir()
+        {
+            Console.WriteLine("ZzzzZZzZZzzzZ");
+        }
+    }
+}
